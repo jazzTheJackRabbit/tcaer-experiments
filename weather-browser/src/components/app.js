67e0c8a7b9e2ba21e3785911ff179401992af563
jazzTheJackRabbit <amogh.91@gmail.com>
2016-12-16 08:50:08 -0800

    New subproject for weather browser

---
+++ weather-browser/src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import {connect} from 'redux'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
