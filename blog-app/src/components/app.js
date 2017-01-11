8ca5d06cf6893d1a929c4fcf6e66b099366e4fa0
jazzTheJackRabbit <amogh.91@gmail.com>
2017-01-10 18:46:00 -0800

    Basic semantic ui stuff

--- blog-app/src/components/app.js
+++ blog-app/src/components/app.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import {connect} from 'redux'
+import {Link} from 'react-router'
 
 export default class App extends Component {
   render() {
     return (
       <div>
-      	Gonna render this for everyone yo!
+      	<div className="ui sticky secondary pointing menu">
+      		<Link className="item" to="/">Home</Link>
+      		<Link className="item" to="/create">Create</Link>
+      	</div>
       	{this.props.children}
       </div>
     );
---
+++ blog-app/src/components/post_create.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class PostCreate extends React.Component{
+	render(){
+		return(
+			<div>
+				This is the creation page
+			</div>
+		)
+	}
+}
+
+export default PostCreate
